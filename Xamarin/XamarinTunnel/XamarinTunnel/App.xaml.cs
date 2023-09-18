119bf5044d36aa8e2846c80ed0574e1e82e4a9eb
lexi-msft <akoopmann@microsoft.com>
2023-09-18 13:43:52 -0700

    Xamarin Sample App

---
+++ Xamarin/XamarinTunnel/XamarinTunnel/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinTunnel
+{
+    public partial class App : Application
+    {
+        public App ()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart ()
+        {
+        }
+
+        protected override void OnSleep ()
+        {
+        }
+
+        protected override void OnResume ()
+        {
+        }
+    }
+}
+
